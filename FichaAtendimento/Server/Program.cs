f7ee234b3348389202b1de26ed2d2aa14d150b7b
Marcio Del Bianco <marcio.bianco@gmail.com>
2024-06-20 11:45:38 -0300

    More refactoring and .NET 8 migration

--- FichaAtendimento/Server/Program.cs
+++ FichaAtendimento/Server/Program.cs
@@ -1,16 +1,15 @@
 ﻿using FichaAtendimento.Server.Data;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+//DI
 builder.Services.AddDbContext<FichaContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLConnection")));
 builder.Services.AddScoped<IFichaDB, FichaDB>();
 builder.Services.AddScoped<IPacienteDB, PacienteDB>();
@@ -20,25 +19,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
+app.UseAuthorization();
 
-app.UseRouting();
-
-
-app.MapRazorPages();
 app.MapControllers();
-app.MapFallbackToFile("index.html");
 
 app.Run();
